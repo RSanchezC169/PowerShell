4c5e5f65cfbadd90e77de9972571727672c2419d
RSanchezC169 <35344339+RSanchezC169@users.noreply.github.com>
2025-03-13 07:27:00 -0600

    Update Load_Module.ps1

--- Random_Functions/Load_Module.ps1
+++ Random_Functions/Load_Module.ps1
@@ -29,66 +29,104 @@ Description of Script:
 ##################################################################################################################################################################
 #==============================Beginning of script================================================================================================================
 ##################################################################################################################################################################
+# Define the global log file path at the start of the script
+$Global:LogFile = "C:\Rsanchezc169ScriptLogs\Log_$(Get-Date -Format 'MM_dd_yyyy_hh_mm_tt').log"
+
+# Global function to handle logging
+Function Write-Log {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory)]
+        [string]$Message,                  # The message to log
+        [Parameter()]
+        [string]$LogFile = $Global:LogFile # Optional: Specify a log file, default to the global log file
+    )
+
+    # Ensure the log file exists
+    $LogDirectory = Split-Path -Path $LogFile
+    if (-not (Test-Path -Path $LogDirectory)) {
+        New-Item -ItemType Directory -Path $LogDirectory -Force | Out-Null
+    }
+
+    # Append the message to the log file with a timestamp
+    $Timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    Add-Content -Path $LogFile -Value "$Timestamp : $Message"
+}
+##################################################################################################################################################################
+##################################################################################################################################################################
 Function Load-Module {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
-        [string[]]$Modules,  # Supports multiple modules
-        [Parameter()]
-        [string]$LogFile = "C:\EmailExportScriptLogs\ScriptActions.log"  # Default log file path
+        [string[]]$Modules  # Supports multiple modules
     )
 
-    # Ensure the directory for the log file exists
-    $LogDirectory = Split-Path -Path $LogFile
-    if (-not (Test-Path -Path $LogDirectory  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue)) {
-        New-Item -ItemType Directory -Path $LogDirectory -Force  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue | Out-Null
-    }
-
     Foreach ($Module in $Modules) {
-        Write-Progress -Activity "Processing Module: $($Module)" -Status "Starting task..." -PercentComplete 0
-
+        Write-Progress -Activity "Processing Module: $($Module)" -Status "Starting task..." -PercentComplete 0 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+        
         Try {
             # Step 1: Check if the module is installed
-            Write-Progress -Activity "Processing Module: $($Module)" -Status "Checking installation status..." -PercentComplete 20
-            if (Get-InstalledModule -Name $Module  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue) {
-                Add-Content -Path $LogFile -Value "Module '$Module' is already installed - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+            Write-Progress -Activity "Processing Module: $($Module)" -Status "Checking installation status..." -PercentComplete 20 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+            if ((Get-InstalledModule -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue) -OR (Get-Module -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue)) {
+                Write-Warning "Module '$Module' is already installed"
+                Write-Log -Message "Module '$Module' is already installed"
             } else {
-                Write-Progress -Activity "Processing Module: $($Module)" -Status "Installing module..." -PercentComplete 40
-                Install-Module -Name $Module -Repository PSGallery -Force -Scope CurrentUser  -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
-                Add-Content -Path $LogFile -Value "Module '$Module' installed successfully - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Write-Progress -Activity "Processing Module: $($Module)" -Status "Installing module..." -PercentComplete 40 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Install-Module -Name "$Module*" -Force -Scope CurrentUser -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Write-Log -Message "Module '$Module' installed successfully"
             }
 
             # Step 2: Check for updates
-            Write-Progress -Activity "Processing Module: $($Module)" -Status "Checking for updates..." -PercentComplete 60
-            $CurrentVersion = (Find-Module -Name $Module  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Version
-            $InstalledVersion = (Get-InstalledModule -Name $Module  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Version
+            Write-Progress -Activity "Processing Module: $($Module)" -Status "Checking for updates..." -PercentComplete 60 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+            $CurrentVersion = (Find-Module -Name "$Module" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Version
+            $InstalledVersion = IF(Get-InstalledModule -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue){(Get-InstalledModule -Name "$Module" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Version} ELSEIF(Get-Module -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue){(Get-Module -Name "$Module" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Version}
+            
             if ($CurrentVersion -and $InstalledVersion -and $CurrentVersion -gt $InstalledVersion) {
-                Write-Progress -Activity "Processing Module: $($Module)" -Status "Updating module..." -PercentComplete 80
-                Update-Module -Name $Module -Force  -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
-                Add-Content -Path $LogFile -Value "Module '$Module' updated successfully - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Write-Progress -Activity "Processing Module: $($Module)" -Status "Updating module..." -PercentComplete 80 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Update-Module -Name "$Module" -Force -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Write-Log -Message "Module '$Module' updated successfully"
             } else {
-                Add-Content -Path $LogFile -Value "Module '$Module' is already up to date - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                Write-Warning "Module '$Module' is up-to-date"
+                Write-Log -Message "Module '$Module' is already up to date"
             }
 
             # Step 3: Import the module
-            Write-Progress -Activity "Processing Module: $($Module)" -Status "Importing module..." -PercentComplete 90
-            Import-Module -Name $Module -Force -Global  -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
-            Add-Content -Path $LogFile -Value "Module '$Module' imported successfully - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+            [ARRAY]$CurrentModules = @()
+            IF (Get-InstalledModule -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue) {
+                $CurrentModules = (Get-InstalledModule -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Name
+            } ELSEIF(Get-Module -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue) {
+                $CurrentModules = (Get-Module -Name "$Module*" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue).Name
+            }
+
+            if ($CurrentModules) {
+                Foreach ($CurrentModule in $CurrentModules) {
+                    Write-Progress -Activity "Processing Sub-Module: $($CurrentModule)" -Status "Importing module..." -PercentComplete 90 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                    
+                    Try {
+                        Import-Module -Name "$CurrentModule" -Force -ErrorAction Stop -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+                        Write-Log -Message "Module '$CurrentModule' imported successfully"
+                    } Catch {
+                        Write-Log -Message "Error importing sub-module '$CurrentModule': $($_.Exception.Message)"
+                        Write-Warning "Error importing sub-module '$CurrentModule'. Check logs for details."
+                    }
+                }
+            } else {
+                Write-Warning "No modules found matching '$Module'. Ensure the module name is correct."
+                Write-Log -Message "No modules found matching '$Module'"
+            }
         } Catch {
             # Log any errors
-            Add-Content -Path $LogFile -Value "Error processing module '$Module': $($_.Exception.Message) - $((Get-Date).ToString("MM/dd/yyyy hh:mm:ss"))"  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
-            #Write-Warning "Error processing module '$Module'. Check logs for details."
+            Write-Log -Message "Error processing module '$Module': $($_.Exception.Message)"
+            Write-Warning "Error processing module '$Module'. Check logs for details."
         }
     }
 
     # Clear the progress bar
-   Write-Progress -Activity "Module Processing" -Status "Completed all tasks." -PercentComplete 100
-   Write-Progress -Activity "Module Processing" -Completed
-
-    #Write-Host "All modules processed. Logs available at: $LogFile"
+    Write-Progress -Activity "Module Processing" -Status "Completed all tasks." -PercentComplete 100 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
+    Write-Progress -Activity "Module Processing" -Completed -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue
 }
-
+##################################################################################################################################################################
 #Usage Examples
 #Load-Module "PowerShellGet"
 #Load-Module "PowerShellGet","Microsoft.Graph"
